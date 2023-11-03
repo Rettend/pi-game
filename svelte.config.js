dec311742bdd7e37a20f6a1533717fd42c3c285f
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-11-03 23:08:29 +0100

    ➕ add: components alias

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,10 @@ const config = {
 		adapter: adapter(),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		},
+    alias: {
+      $components: 'src/components',
+    }
 	}
 }
 
