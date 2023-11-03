00d43b527fafd5e95dd9a3505f0b7391ae69712f
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-11-03 23:28:31 +0100

    🔧 fix: add base to routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import 'uno.css'
+	import { base } from '$app/paths'
+	import 'uno.css'
 </script>
 
 <main class="text-center p-1em my-0 mx-auto font-default">
@@ -7,8 +8,8 @@
 	<!-- show which one is active and be centered -->
 
 	<nav class="flex justify-center">
-		<a href="/" class="p-1em">Game</a>
-		<a href="/about" class="p-1em">About</a>
+		<a href="{base}/" class="p-1em">Game</a>
+		<a href="{base}/about" class="p-1em">About</a>
 	</nav>
 
 	<slot />
